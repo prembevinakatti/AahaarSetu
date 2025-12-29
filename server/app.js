759e9f676323496dcd6ba83c272039ecb5d989e9
test <test@gmail.com>
2025-12-29 21:44:08 +0530

    ..

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-asd
+const express=require ("express");
+const connectDB = require("./config/database");
+const app=express();
+require("dotenv").config();
+
+
+
+const PORT=process.env.PORT||8000
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running on PORT ${PORT}`)
+})
--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,15 @@
-asd
+const { default: mongoose } = require("mongoose")
+
+const connectDB=async (req,res)=>{
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI).then(()=>{
+            console.log("Connection successfull")
+        })
+
+        
+    } catch (error) {
+        console.log("error in connecting to MongoDB",error)
+    }
+}
+module.exports=connectDB;
