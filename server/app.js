57a496b56b7eeec70729e8d79854c3a40dfe6e53
prem <prem@prem.com>
2025-12-29 21:52:36 +0530

    ..

--- server/app.js
+++ server/app.js
@@ -1,21 +1,16 @@
-const express=require ("express");
+const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 
-
-
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const PORT = process.env.PORT || 8080;
 
-
-
-const PORT=process.env.PORT||8000
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on PORT ${PORT}`);
+});
--- server/controllers/volunteerAuth.controller.js
+++ server/controllers/volunteerAuth.controller.js
@@ -1,1 +1,21 @@
-ad
+const volunteerAuthModel = require("../models/volunteerAuth.model");
+
+module.exports.registerVolunteer = async (req, res) => {
+  try {
+    const { username, email, phoneNumber, password } = req.body;
+
+    if (!username || !email || !phoneNumber || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingVolunteer = await volunteerAuthModel.findOne({ email });
+    if (esistingVolunteer) {
+      return res
+        .status(400)
+        .json({ message: "Volunteer with this email already exists" });
+    }
+  } catch (error) {
+    console.log("Error registering volunteer:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
