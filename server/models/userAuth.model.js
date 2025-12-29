0a9832584a3597801b53b9000a24a744387fe33f
test <test@gmail.com>
2025-12-29 21:59:27 +0530

    ..

--- server/models/userAuth.model.js
+++ server/models/userAuth.model.js
@@ -1,1 +1,26 @@
-asd
+const mongoose=require ("mongoose");
+const bcrypt=require("bcrypt");
+const userauthSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+userauthSchema.pre("save",async function(next){
+    if(!this.isModified("password")) 
+    return next();
+const hash = await bcrypt.hash(this.password,10)
+    this.password=hash;
+    next();
+
+})
+module.exports=mongoose.model("User",userauthSchema);
