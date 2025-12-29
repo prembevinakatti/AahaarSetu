0f20cd6f45b78ba5e8faefa447b132bf87cb3d7b
prem <prem@prem.com>
2025-12-29 21:43:24 +0530

    ..

--- server/models/adminAuth.model.js
+++ server/models/adminAuth.model.js
@@ -1,1 +1,36 @@
-asd
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const adminAuthSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  orgName: {
+    type: String,
+    required: true,
+  },
+  orgNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+adminAuthSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+module.exports = mongoose.model("AdminAuth", adminAuthSchema);
--- server/models/volunteerAuth.model.js
+++ server/models/volunteerAuth.model.js
@@ -1,1 +1,32 @@
-asd
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const volunteerAuthSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+volunteerAuthSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const hash = await bcrypt.has(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+module.exports = mongoose.model("VolunteerAuth", volunteerAuthSchema);
