840e03305a940403ab07c2f11c151570b6677dc0
MeghanaKotambari <meghanakotambari@gmail.com>
2026-01-03 21:39:39 +0530

    ..

--- client/src/Routers/AppRoutes.jsx
+++ client/src/Routers/AppRoutes.jsx
@@ -1,15 +1,15 @@
 import LandingPage from '@/components/LandingPage'
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
 
 const AppRoutes = () => {
   return (
     <div>
-     <Router/>
+  
         <Routes>
             <Route path='/' element={<LandingPage/>}/>
         </Routes>
-        <Router/>
+       
    
     </div>
   )
