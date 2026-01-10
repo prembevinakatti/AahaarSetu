47adac9ce9f0d4c4b7005c4a27f5ed1a05473a5b
OnkarBevinakatti <onkarbevinakatti09@gmail.com>
2026-01-10 18:57:19 +0530

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "leaflet/dist/leaflet.css"
-
+import "leaflet/dist/leaflet.css";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
---
+++ client/src/store/authSlice.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+  },
+  reducers: {
+    setAuthUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+  },
+});
+
+export const { setAuthUser } = authSlice.actions;
+export default authSlice.reducer;
